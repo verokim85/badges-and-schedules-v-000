151a308993bbc9daaca938c86f1616e79f90998e
Veronica Kim <vero.a.kim@gmail.com>
2018-01-21 19:12:08 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  new_array = []
+  name.each do |speaker|
+    new_array << "Hello, my name is #{speaker}."
+    end
+    new_array
+end
+
+def assign_rooms(name)
+  new_arr = []
+  name.each_with_index do |element, index|
+    new_arr <<"Hello, #{element}! You'll be assigned to room #{index+1}!"
+  end
+  new_arr
+end
+
+def printer(array)
+
+  batch_badge_creator(array).each do |name|
+    puts name
+  end
+
+  assign_rooms(array).each do |rooms|
+    puts rooms
+  end
+
+end
